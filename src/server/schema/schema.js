c57478608d7aeb1be325396335cdbc8e9a137fb8
Avishek Chatterjee <cavishek39@gmail.com>
2021-03-25 15:57:54 +0530

    author resolver added

--- src/server/schema/schema.js
+++ src/server/schema/schema.js
@@ -2,7 +2,14 @@
 const _ = require('loadsh')
 const graphql = require('graphql')
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLID,
+} = graphql
 
 // Dummy Data
 
@@ -24,11 +31,32 @@ const books = [
   },
 ]
 
+const authors = [
+  {
+    id: '1',
+    name: 'ABC',
+    age: 18,
+    mobile_no: 7897979793,
+  },
+  {
+    id: '2',
+    name: 'ABC',
+    age: 12,
+    mobile_no: 3897979793,
+  },
+  {
+    id: '3',
+    name: 'ABC',
+    age: 14,
+    mobile_no: 3897979745,
+  },
+]
+
 // Define our book type
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
@@ -38,10 +66,10 @@ const BookType = new GraphQLObjectType({
 const AuthorType = new GraphQLObjectType({
   name: 'Author',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
-    mobile_no: { type: GraphQLInt },
+    mobile_no: { type: GraphQLFloat },
   }),
 })
 
@@ -61,10 +89,18 @@ const RootQuery = new GraphQLObjectType({
        * if he/she want to fecth a book by it's 'id' on the parameter then
        * the args property will come into play.
        */
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from the db/ other source
         return _.find(books, { id: args.id })
+      },
+    },
+
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id })
       },
     },
   },
